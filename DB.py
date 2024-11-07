189a32f0399582202a83410b8d1a0f0cf9c6257a
우성윤 <tjddbsghfhcom@naver.com>
2024-11-07 11:10:01 +0900

    Database
    Cloud MongoDB 데이터 삽입

--- DB.py
+++ DB.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def DB_insert(lat, lng, status, image_path, loc, damage):
     # 연결
-    client = MongoClient(f"mongodb+srv://21928296:n9Dx8ngcU2pD0lyj@database.2vxog.mongodb.net/?retryWrites=true&w=majority&appName=database")
+    client = MongoClient(f"Mongo DB 연결 Key")
     db = client['database']
     collection = db['myCollection']  # 사용할 컬렉션 선택
 
